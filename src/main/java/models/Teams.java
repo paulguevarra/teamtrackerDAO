cf021f300b573f79172f594924627a0d728affe4
Paul Guevarra <p.a.guevarra@gmail.com>
2017-08-11 16:06:24 -0700

    create a static method to return all Teams and a second method to clear all Teams with tests to check each moethod for functionality

--- src/main/java/models/Teams.java
+++ src/main/java/models/Teams.java
@@ -1,14 +1,23 @@
 package models;
 
 import java.security.PrivateKey;
+import java.util.ArrayList;
 
 public class Teams {
     private String teamName;
+    private static ArrayList<Teams> instances=new ArrayList<>();
 
     public Teams(String teamName){
         this.teamName=teamName;
+        instances.add(this);
     }
     public String getTeamName(){
         return teamName;
+    }
+    public static ArrayList<Teams> getAll(){
+        return instances;
+    }
+    public static void clearAllTeams(){
+        instances.clear();
     }
 }
--- src/test/java/models/TeamsTest.java
+++ src/test/java/models/TeamsTest.java
@@ -10,7 +10,10 @@ public class TeamsTest {
     @Before
     public void setUp() throws Exception {
     }
-
+    @After
+    public void tearDown() throws Exception {
+        Teams.clearAllTeams();
+    }
     @Test
     public void newTeamsObjectGetsCreatedCorrectly_true() throws Exception {
         Teams teams = new Teams("TeamFusion");
@@ -23,8 +26,18 @@ public class TeamsTest {
         assertEquals("TeamFusion", teams.getTeamName());
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void allTeamsAreCorrectlyReturned_true(){
+        Teams teams = new Teams("TeamFusion");
+        Teams nextTeams = new Teams("Dragon Fever");
+        assertEquals(2,Teams.getAll().size());
     }
 
+    @Test
+    public void allTeamsContainsAllTeams_true() {
+        Teams teams = new Teams("TeamFusion");
+        Teams nextTeams = new Teams("Dragon Fever");
+        assertTrue(Teams.getAll().contains(teams));
+        assertTrue(Teams.getAll().contains(nextTeams));
+    }
 }
