cc186dab1a154a0abd46648a1744d8f757509a94
Paul Guevarra <p.a.guevarra@gmail.com>
2017-08-11 14:50:09 -0700

    create test to check if new objects are created correctly

--- src/test/java/models/TeamsTest.java
+++ src/test/java/models/TeamsTest.java
@@ -2,12 +2,19 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TeamsTest {
     @Before
     public void setUp() throws Exception {
+    }
+
+    @Test
+    public void newTeamsObjectGetsCreatedCorrectly_true() throws Exception {
+        Teams teams = new Teams("TeamFusion");
+        assertEquals(true, teams instanceof Teams);
     }
 
     @After
