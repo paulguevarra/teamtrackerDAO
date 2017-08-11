4ac25077bd452044a1ce8671a4a3ce9076851e42
Paul Guevarra <p.a.guevarra@gmail.com>
2017-08-11 15:33:53 -0700

    create post route for teamName

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
+import models.Teams;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -12,6 +13,13 @@ public class App {
         get("/", (request, response) -> {
             Map<String,Object> model=new HashMap<>();
             return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/teams/new", (request, response) -> {
+            Map<String,Object> model=new HashMap<>();
+            String teamName=request.queryParams("teamName");
+            Teams newTeam=new Teams("teamName");
+            return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
